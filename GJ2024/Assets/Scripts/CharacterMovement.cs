4f55e05fbeaa93dc0a2b7ad84b0a3e50d8ebdae1
Bluecatto <asrampersad@st.noorderpoort.nl>
2024-02-12 12:24:04 +0100

    Updated character movement

--- GJ2024/Assets/Scripts/CharacterMovement.cs
+++ GJ2024/Assets/Scripts/CharacterMovement.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public CharacterController characterController;
-    public float Speed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
 
     void Start()
     {
-       characterController = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        characterController.Move(move * Time.deltaTime * Speed);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift); 
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        float speed = isRunning ? runSpeed : walkSpeed;
+
+        characterController.Move(moveDirection * Time.deltaTime * speed);
     }
 }
