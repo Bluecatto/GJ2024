02e3f224654451a14da5d4e2e83dec4a79982cda
Bluecatto <asrampersad@st.noorderpoort.nl>
2024-02-15 21:15:46 +0100

    Changing a biy

--- GJ2024/Assets/Scripts/movement.cs
+++ GJ2024/Assets/Scripts/movement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public Animator anim;
     private float horizontalInput;
     private float verticalInput;
     [SerializeField] private float speed;
@@ -18,12 +17,7 @@ public class movement : MonoBehaviour
 
         if (horizontalInput != 0 || verticalInput != 0)
         {
-            anim.SetBool("isWalking", true);
             transform.position += new Vector3(movementInput.x * speed * 0.025f, 0, movementInput.y * speed * 0.025f);
-        }
-        else
-        {
-            anim.SetBool("isWalking", false);
         }
     }
 }
